32ce7c143eb4f2fe9a3a05e283e8768910bfbd9b
Andre Oliveira <andreoliveirar2d2@gmail.com>
2025-07-20 10:10:04 -0300

    fix: add and loading fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import {
+  useFonts,
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+} from '@expo-google-fonts/host-grotesk';
+import * as SplashScreen from 'expo-splash-screen';
 
 import './styles/global.css';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
   return (
     <View className="bg-lime-500 flex-1 items-center justify-center">
       <Text className="text-base">Open up App.tsx to start working on your app!</Text>
