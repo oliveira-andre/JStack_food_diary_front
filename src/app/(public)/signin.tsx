b6d5534325605daa412fa613a40c24dd5583be76
Andre Oliveira <andreoliveirar2d2@gmail.com>
2025-07-20 15:45:41 -0300

    sign in page

--- src/app/(public)/signin.tsx
+++ src/app/(public)/signin.tsx
@@ -1,37 +1,45 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { router } from 'expo-router';
+import { ArrowLeftIcon } from 'lucide-react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { AuthLayout } from '../../components/AuthLayout';
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+import { colors } from '../../styles/colors';
 
 export default function SignIn() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Signin</Text>
-        <Link href="/">
-        <Text>Back to home</Text>
-        </Link>
+    <AuthLayout
+      icon="👤"
+      title="Entre em sua conta"
+      subtitle="Acesse sua conta para continuar"
+    >
+      <View className="justify-between flex-1">
+        <View className="gap-6">
+          <Input
+            label="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoComplete="email"
+          />
+        
+          <Input
+            label="Senha"
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoComplete="password"
+            secureTextEntry
+          />
+        </View>
+
+        <View className="flex-row gap-6">
+          <Button onPress={router.back} size="icon" color="gray">
+            <ArrowLeftIcon size={20} color={colors.black[700]} />
+          </Button>
+          <Button className="flex-1">Entrar</Button>
+        </View>
       </View>
-    </View>
+    </AuthLayout>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
